620d93e891e6d5ee570148dbcea8a204d568f0a6
hemangini477 <hemanginipatel022@gmail.com>
2024-11-03 21:57:11 -0500

    commit changes

---
+++ src/Components/ShoppingCart.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ShoppingCart.css";
+const ShoppingCart = () => {
+  return (
+    <>
+      <div className="shoppiong-cart">
+        <h2 className="shopping-cart-title">Shopping Cart</h2>
+        <ul className="cart-items"></ul>
+        <button className="clear-cart-btn">Clear Cart</button>
+      </div>
+    </>
+  );
+};
+
+export default ShoppingCart;
